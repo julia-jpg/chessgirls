af37021dc921f472566a724ddf954dc5a6f88667
julia-jpg <julia.escalante@fundacion-jala.org>
2020-08-13 00:51:10 -0400

    add files to chess

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    private String hi;
+    public Main() {
+    }
+
+    /**
+     * @Return string
+     * */
+    public String getHi() {
+        return hi;
+    }
+    /**
+     * @param
+     * */
+    public void setHi(final String hii) {
+        this.hi = hii;
+    }
+}
